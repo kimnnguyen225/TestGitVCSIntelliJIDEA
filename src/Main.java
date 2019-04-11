eb4aaf9b89be8e41a249182c55f53c7041d6217c
Kim Nguyen <kimnnguyen225@gmail.com>
2019-04-10 21:13:19 -0400

    Initial Commit: print 3 lines Success!

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Success!");
+        System.out.println("Success!");
+        System.out.println("Success!");
+    }
+}
